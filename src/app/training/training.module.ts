b025d0c35a63a9a88cd5d4b210cf09d66e42d6b1
Artifex-Latronis <Artifex-Latronis@users.noreply.github.com>
2018-08-27 08:16:22 -0700

    Optimized Subscriptions

---
+++ src/app/training/training.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrainingComponent } from './training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingComponent } from './past-training/past-training.component';
+import { StopTrainingComponent } from './current-training/stop-training.component';
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AngularFirestoreModule,
+    FormsModule
+  ],
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    StopTrainingComponent
+  ],
+  entryComponents: [StopTrainingComponent]
+})
+export class TrainingModule { }
